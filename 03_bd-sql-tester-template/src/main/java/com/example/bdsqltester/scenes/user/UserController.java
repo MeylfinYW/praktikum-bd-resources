f99568bdde59c53f132c286e07e127b163ed5551
Meylfin <meylfinwijaya1013@gmail.com>
2025-05-14 20:54:32 +0700

    Ubahan Kode

--- 03_bd-sql-tester-template/src/main/java/com/example/bdsqltester/scenes/user/UserController.java
+++ 03_bd-sql-tester-template/src/main/java/com/example/bdsqltester/scenes/user/UserController.java
@@ -1,5 +1,26 @@
 package com.example.bdsqltester.scenes.user;
 
-public class UserController {
+import com.example.bdsqltester.datasources.MainDataSource;
+import com.example.bdsqltester.dtos.Assignment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
-}
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserController {
+    @FXML
+    private ListView<Assignment> assignmentList;
+    @FXML private TextField idField;
+    @FXML private TextField nameField;
+    @FXML private TextArea instructionsField;
+    @FXML private TextArea answerKeyField;
+    @FXML private TextArea userQueryArea;
+    @FXML private Label gradeLabel;
+
+    Connection connection = MainDataSource.getConnection();
